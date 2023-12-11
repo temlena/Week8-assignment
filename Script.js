3c603a3fdf1ba466f29efcdbd0e20852ab332f54
Helen Oridupa <94105502+temlena@users.noreply.github.com>
2023-12-11 04:08:08 +0100

    Initial commit

---
+++ Script.js
@@ -0,0 +1,10 @@
+// Function that reverses a string
+function reverseString(str) {
+    let newString = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+}
+reverseString('Helen');
+//Output "neleH"
